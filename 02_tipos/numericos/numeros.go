a03ca37bed27f50fba1a6ec6eee2e40a0e123c5f
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-23 17:43:19 -0300

    tipos numéricos

---
+++ 02_tipos/numericos/numeros.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1 + 2.5 =", 1+2.5)
+	fmt.Println("10 / 3 =", 10/3)
+	fmt.Println("10 / 3.0 =", 10/3.0)
+	fmt.Println("5 % 3 =", 5%3)
+	fmt.Println("100 * 0.75 =", 100*0.75)
+	fmt.Printf("TIPO de 100 * 0.75 = %T \n", 100*0.75)
+
+}
